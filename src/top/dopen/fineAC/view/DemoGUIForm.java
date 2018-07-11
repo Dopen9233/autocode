d850ea946f4354d78b359556010d0a86ace07368
苟开唯 <923316226@qq.com>
2018-07-11 15:21:14 +0800

    添加POI工具，编写word文档生成过程demo

---
+++ src/top/dopen/fineAC/view/DemoGUIForm.java
@@ -0,0 +1,19 @@
+package top.dopen.fineAC.view;
+
+import javax.swing.*;
+
+/**
+ * @author 苟开唯
+ * @version V1.0
+ * @Title: ${file_name}
+ * @Package top.dopen.fineAC.view
+ * @Description: ${TODO}
+ * @date 2018/7/3 17:17
+ */
+public class DemoGUIForm {
+    private JPanel 测试;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
